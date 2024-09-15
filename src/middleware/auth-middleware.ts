6545b1d804231719189c8a22023166e962143b0a
Gmedulla101 <edosegheosamagbe@gmail.com>
2024-09-15 21:30:56 +0100

    Finalised auth middleware

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -6,13 +6,18 @@ const auth = async (req, res, next) => {
   const authHeader = req.body;
   if (!authHeader || !authHeader.startsWith('Bearer')) {
     throw new UnauthenticatedError('Authentication invalid');
-  };
+  }
 
   const token = authHeader.split(' ')[1];
 
   try {
-    
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = {
+      username: payload.username,
+      email: payload.email,
+      id: payload.userId,
+    };
   } catch (error) {
-    
+    throw new UnauthenticatedError("Authentication invalid")
   }
 };
