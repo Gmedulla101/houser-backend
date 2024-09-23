3efb885e048b16834cb23a0138c67468bc96e206
Gmedulla101 <edosegheosamagbe@gmail.com>
2024-09-23 01:48:25 +0100

    Refactored endpoints with express async handler

--- src/controllers/properties-controller.ts
+++ src/controllers/properties-controller.ts
@@ -1,85 +1,69 @@
 import propertyModel from '../models/Properties-model';
+import asyncHandler from 'express-async-handler';
 import { StatusCodes } from 'http-status-codes';
 import { NotFoundError, UnauthenticatedError } from '../errors';
 
-const getAllProps = async (req, res, next) => {
-  try {
-    const props = await propertyModel.find({});
-    res
-      .status(StatusCodes.OK)
-      .json({ success: true, data: props, nbHits: props.length });
-  } catch (err) {
-    next(err);
-  }
-};
+const getAllProps = asyncHandler(async (req, res) => {
+  const props = await propertyModel.find({});
+  res
+    .status(StatusCodes.OK)
+    .json({ success: true, data: props, nbHits: props.length });
+});
 
-const getProp = (req, res) => {
-  res.send('Getting property');
-};
+const getProp = asyncHandler(async (req, res) => {
+  const _id = req.params.id;
+  const prop = await propertyModel.findOne({ _id });
+  res.status(StatusCodes.OK).json({ success: true, data: prop });
+});
 
-const getUserProp = async (req, res, next) => {
-  try {
-    const userProps = await propertyModel.find({ createdBy: req.user.userId });
-    res
-      .status(StatusCodes.OK)
-      .json({ success: true, data: userProps, nbHits: userProps.length });
-  } catch (err) {
-    next(err);
-  }
-};
+const getUserProp = asyncHandler(async (req: any, res) => {
+  const userProps = await propertyModel.find({ createdBy: req.user.userId });
+  res
+    .status(StatusCodes.OK)
+    .json({ success: true, data: userProps, nbHits: userProps.length });
+});
 
-const createProp = async (req, res, next) => {
+const createProp = asyncHandler(async (req: any, res, next) => {
   req.body.createdBy = req.user.userId;
-  try {
-    const newProperty = await propertyModel.create({ ...req.body });
-    res.status(StatusCodes.OK).json({ sucess: true, data: newProperty });
-  } catch (err) {
-    next(err);
-  }
-};
 
-const deleteProp = async (req, res, next) => {
+  const newProperty = await propertyModel.create({ ...req.body });
+  res.status(StatusCodes.OK).json({ sucess: true, data: newProperty });
+});
+
+const deleteProp = asyncHandler(async (req: any, res) => {
   const _id = req.params.id;
 
-  try {
-    const deletedProp = await propertyModel.findOneAndDelete(
-      { _id, createdBy: req.user.userId },
-      req.body
+  const deletedProp = await propertyModel.findOneAndDelete(
+    { _id, createdBy: req.user.userId },
+    req.body
+  );
+
+  if (!deletedProp) {
+    throw new NotFoundError(
+      `Listing with ${_id} does not exist or you are not authorised to delete this listing`
     );
-
-    if (!deletedProp) {
-      throw new NotFoundError(
-        `Listing with ${_id} does not exist or you are not authorised to delete this listing`
-      );
-    }
-
-    res.status(StatusCodes.OK).json({ success: true, data: deletedProp });
-  } catch (err) {
-    next(err);
   }
-};
 
-const updateProp = async (req, res, next) => {
+  res.status(StatusCodes.OK).json({ success: true, data: deletedProp });
+});
+
+const updateProp = asyncHandler(async (req: any, res) => {
   const _id = req.params.id;
 
-  try {
-    const updatedProp = await propertyModel.findOneAndUpdate(
-      { _id, createdBy: req.user.userId },
-      req.body,
-      { new: true }
+  const updatedProp = await propertyModel.findOneAndUpdate(
+    { _id, createdBy: req.user.userId },
+    req.body,
+    { new: true }
+  );
+
+  if (!updatedProp) {
+    throw new NotFoundError(
+      `Listing with ${_id} does not exist or you are not authorised to edit this listing`
     );
-
-    if (!updatedProp) {
-      throw new NotFoundError(
-        `Listing with ${_id} does not exist or you are not authorised to edit this listing`
-      );
-    }
-
-    res.status(StatusCodes.OK).json({ success: true, data: updatedProp });
-  } catch (err) {
-    next(err);
   }
-};
+
+  res.status(StatusCodes.OK).json({ success: true, data: updatedProp });
+});
 
 export {
   getAllProps,
