94a59be2295169e03293be8baf7581fd117045a4
Gmedulla101 <edosegheosamagbe@gmail.com>
2024-11-12 17:21:12 +0100

    Completed user controller

--- src/routes/auth-route.ts
+++ src/routes/auth-route.ts
@@ -1,17 +1,10 @@
 import express from 'express';
-import {
-  register,
-  login,
-  getUser,
-  checkUser,
-} from '../controllers/auth-controller';
+import { register, login } from '../controllers/auth-controller';
 import { auth } from '../middleware/auth-middleware';
 
 const authRouter = express.Router();
 
 authRouter.post('/register-user', register);
 authRouter.post('/user-login', login);
-authRouter.get('/getUser', auth, getUser);
-authRouter.get('/checkUser', checkUser);
 
 export default authRouter;
--- src/routes/user-route.ts
+++ src/routes/user-route.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import { checkUser, getUser } from '../controllers/user-controller';
+import { auth } from '../middleware/auth-middleware';
 
 const userRouter = Router();
+
+userRouter.get('/getUser', auth, getUser);
+userRouter.get('/checkUser', checkUser);
 
 export default userRouter;
